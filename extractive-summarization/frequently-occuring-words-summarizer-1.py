40e525604f88b94cd45e61fcbe47728a36602d91
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-02-26 17:30:31 +0500

    extractive summarizer added

---
+++ extractive-summarization/frequently-occuring-words-summarizer-1.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import re
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+import hashlib
+import csv
+
+
+def _remove_stopwords(sen):
+    sen_new = " ".join([i for i in sen if i not in stop_words])
+    return sen_new
+
+
+def _create_dictionary_table(sentences) -> dict:
+    frequency_table = dict()
+    stop_words = set(stopwords.words("english"))
+    stem = PorterStemmer()
+    for text_string in sentences:
+        words = word_tokenize(text_string)
+        for wd in words:
+            wd = stem.stem(wd)
+            if wd in stop_words:
+                continue
+            if wd in frequency_table:
+                frequency_table[wd] += 1
+            else:
+                frequency_table[wd] = 1
+    return frequency_table
+
+
+def _calculate_sentence_scores(sentences, frequency_dictionary) -> dict:
+    _sentence_with_scores = dict()
+    stem = PorterStemmer()
+    stop_words = set(stopwords.words("english"))
+    for text_string in sentences:
+        score = 0
+        number_of_non_stopwords = 0
+        words = word_tokenize(text_string)
+        for wd in words:
+            wd = stem.stem(wd)
+            if wd in stop_words:
+                continue
+            if re.match(r'^[A-Za-z0-9\s]+$',wd):
+                number_of_non_stopwords += 1
+                score += frequency_dictionary[wd]
+        score = (score + 0.5) / (number_of_non_stopwords + 1)
+        _sentence_with_scores[text_string] =  score
+    return _sentence_with_scores
+
+
+def _calculate_average_score(sentence_weight) -> int:
+    sum_values = 0
+    for entry in sentence_weight:
+        sum_values += sentence_weight[entry]
+    average_score = (sum_values / len(sentence_weight))
+    return average_score
+
+
+def _get_meeting_summary(sentences, sentence_weight, threshold):
+    sentence_counter = 0
+    meeting_summary = ''
+
+    for sentence in sentences:
+        # sentence_hash = hashlib.md5(sentence.encode())
+        sentence_hash = sentence
+        if sentence_hash in sentence_weight and sentence_weight[sentence_hash] >= (threshold):
+            meeting_summary += " " + sentence
+            sentence_counter += 1
+    return meeting_summary
+
+
+def sorted_weighted_frequency_sentences(sentences):
+    # creating a dictionary for the word frequency table
+    frequency_table = _create_dictionary_table(sentences)
+
+    # algorithm for scoring a sentence by its words
+    sentence_scores = _calculate_sentence_scores(sentences, frequency_table)
+    sorted_sentence_scores = sorted([(key, value) for (key, value) in sentence_scores.items()], key=lambda x: x[1],reverse=False)
+
+    # getting the threshold
+    threshold = _calculate_average_score(sentence_scores)
+
+    # #producing the summary
+    meeting_summary = _get_meeting_summary(sentences, sentence_scores, 1.5 * threshold)
+
+    return sorted_sentence_scores, meeting_summary
+
+
+if __name__ == '__main__':
+
+    with open('data_meeting_text_amazon.txt') as data_file:
+        _data = json.load(data_file)
+        sentences = [d['sentence'].lower() for d in _data]
+        speakers = [d['speaker'] for d in _data]
+
+    stop_words = stopwords.words('english')
+
+    sentence_ranking, summary = sorted_weighted_frequency_sentences(sentences)
+
+    with open('ranked_sentences_frequency_based1.csv', 'w') as output_file:
+        writer = csv.writer(output_file)
+        for sen in sentence_ranking:
+            writer.writerow(list(sen))
+
+    with open('summary_frequency_based1.txt', 'w') as output_file:
+        output_file.write(summary)
---
+++ extractive-summarization/page-rank-summarizer.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import re
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+import hashlib
+import csv
+
+
+def _remove_stopwords(sen):
+    sen_new = " ".join([i for i in sen if i not in stop_words])
+    return sen_new
+
+
+def _get_word_embeddings(model_name):
+    word_embeddings = dict()
+    f = open(model_name, encoding='utf-8')
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        word_embeddings[word] = coefs
+    f.close()
+    return word_embeddings
+
+
+def _get_sentence_vectors(word_embeddings, sentences, dim):
+    sentence_vectors = list()
+    for i in clean_sentences:
+        if len(i) != 0:
+            v = sum([word_embeddings.get(w, np.zeros((dim,))) for w in i.split()]) / (len(i.split()) + 0.001)
+        else:
+            v = np.zeros((dim,))
+        sentence_vectors.append(v)
+    return sentence_vectors
+
+
+def _get_similarity_matrix(size, sentence_vectors, dim):
+    sim_mat = np.zeros([size, size])
+    for i in range(size):
+        for j in range(size):
+            if i != j:
+                sim_mat[i][j] = \
+                    cosine_similarity(sentence_vectors[i].reshape(1, dim), sentence_vectors[j].reshape(1, dim))[0, 0]
+    return sim_mat
+
+
+def _get_network_graph_scores(sim_mat):
+    nx_graph = nx.from_numpy_array(sim_mat)
+    scores = nx.pagerank(nx_graph)
+    return scores
+
+
+def _get_sentence_ranking(sentences, ng_scores):
+    ranked_sentences = sorted(((ng_scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+    return ranked_sentences
+
+
+def page_rank_algorithm(model_file, dim):
+    word_embeddings = _get_word_embeddings(model_file)
+    sentence_vectors = _get_sentence_vectors(word_embeddings, clean_sentences, dim)
+    similarity_vector = _get_similarity_matrix(len(sentences), sentence_vectors, dim)
+    network_graph_score = _get_network_graph_scores(similarity_vector)
+    sentence_ranking = _get_sentence_ranking(sentences, network_graph_score)
+    return sentence_ranking
+
+
+if __name__ == '__main__':
+
+    nltk.download('stopwords')# one time execution
+    nltk.download('punkt') # one time execution
+
+    with open('data_meeting_text_amazon.txt') as data_file:
+        _data = json.load(data_file)
+        sentences = [d['sentence'].lower() for d in _data]
+        speakers = [d['speaker'] for d in _data]
+
+    stop_words = stopwords.words('english')
+    clean_sentences = [_remove_stopwords(r.split()) for r in sentences]
+    sentence_ranking = page_rank_algorithm('glove.6B.100d.txt', 100)
+
+    with open('ranked_sentences_100d.csv', 'w') as output_file:
+        writer = csv.writer(output_file)
+        for sen in sentence_ranking:
+            writer.writerow(list(sen))
