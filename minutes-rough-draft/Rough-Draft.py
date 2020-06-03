a6774f87103c24e8f6e43ac5bf9135bdc5f0ef3a
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-06-03 17:49:32 +0500

    added question answer pairng in rough draft

--- minutes-rough-draft/Rough-Draft.py
+++ minutes-rough-draft/Rough-Draft.py
@@ -11,18 +11,33 @@ def get_file_content(filename: str) -> json:
 if __name__ == '__main__':
     clusters = get_file_content('clusters.txt')
     importants = get_file_content('important-sentence.txt')
+    question_answers = get_file_content('question-answer-pairs.txt')
+
+    important_question_answers_id_map = {qa['dialogue_id']: qa for qa in question_answers}
 
     resultants = {}
     for i in range(0, len(clusters)):
         cluster_data = clusters[i]
         important_data = importants[i]
+        id = important_data['dialogue_id']
 
         cluster = cluster_data['cluster']
         classification_type = str(important_data['classification_type'])
         if classification_type.startswith("Interrogative"):
             classification_type = "Questions Answers"
 
-        if important_data['is_important']:
+        if classification_type == "Questions Answers" and id in important_question_answers_id_map:
+            question_answer = important_question_answers_id_map[id]
+            if cluster not in resultants:
+                resultants[cluster] = {'Normal': [], 'Suggestion': [], 'Task Assigned': [], 'Questions Answers': []}
+            resultants[cluster][classification_type].append({'dialogue_id': important_data['dialogue_id'],
+                                                             'sentence': question_answer['question'],
+                                                             'speaker': important_data['speaker'],
+                                                             'score': important_data['score'],
+                                                             'cluster': cluster,
+                                                             'result': question_answer['result'],
+                                                             'classification_type': classification_type})
+        elif important_data['is_important'] and classification_type != "Questions Answers":
             if cluster not in resultants:
                 resultants[cluster] = {'Normal': [], 'Suggestion': [], 'Task Assigned': [], 'Questions Answers': []}
             resultants[cluster][classification_type].append({'dialogue_id': important_data['dialogue_id'],
